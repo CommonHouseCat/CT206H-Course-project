6803e35e192da949b4f0da2e16158b893d85ba6a
Nguyen Quang Vinh <dev.nguyenquangvinh@gmail.com>
2023-11-02 21:13:38 +0700

    Add some resourses

--- src/StudentManagement/Main.java
+++ src/StudentManagement/Main.java
@@ -3,15 +3,48 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.sql.ResultSet;
 import java.util.Scanner;
 import java.sql.PreparedStatement;
 import java.sql.CallableStatement;
 
 
+
 public class Main {
 
 	public static void main(String[] args) {
+		Connection conn = null;
+		Statement stmt = null;
+		ResultSet rs = null;
 		
+		try{
+			Class.forName("com.mysql.cj.jdbc.Driver").newInstance();
+			String connStr = "jdbc:mysql://localhost/StudentManagement?user=project&password=123";
+			conn = DriverManager.getConnection(connStr);
+			System.out.println("Connected!");
+			
+			stmt = conn.createStatement();
+			String sql = "Select * from Student";
+			rs = stmt.executeQuery(sql);
+			System.out.println("Query executed");
+			
+			System.out.println("-------List of Student-------");
+			while(rs.next()) {
+				System.out.println("Student's ID: " + rs.getString("sID"));
+				System.out.println("Student's name: " + rs.getString("sName"));
+				System.out.println("Student's sex: " + rs.getString("sex"));
+				System.out.println("Student's department ID: " + rs.getString("dID"));
+				System.out.println("Student's address: " + rs.getString("address"));
+				System.out.println("------------------------------");
+				System.out.println("");
+			}
+			
+			
+			
+			
+		}catch(Exception e) {
+			System.out.println("Cannot connect:" + e.getMessage());
+		}
 		
 	}
 
